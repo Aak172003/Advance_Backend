551eac9a96a3516cf15316ce3c6f0f09c1f794ff
Aak172003 <prajapatiaakash364@gmail.com>
2024-03-30 00:26:23 +0530

    create getAllVideo_with_query update_thumbnail update_video_thumbnail

--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -11,6 +11,7 @@ const videoRouter = Router()
 
 videoRouter.route('/').get(videoController.getAllVideos)
 
+
 videoRouter.use(verifyJWT)   // Apply verifyJWT middleware to all routes in this file
 
 videoRouter.route('/').post(upload.fields([
@@ -22,6 +23,24 @@ videoRouter.route('/').post(upload.fields([
         maxCount: 1
     }
 ]), videoController.publishAVideo)
+
+videoRouter.route('/getvideo/:videoId').get(videoController.getVideoById)
+
+// Update Only Thumbnail in this Controller 
+// videoRouter.route('/update/:videoId').patch(upload.single('thumbnailLocalPath'), videoController.updateThumbnail)
+
+// Update Thumbnail And Video in single Controller 
+videoRouter.route('/update/:videoId').patch(upload.fields([
+    {
+        name: "thumbnailLocalPath",
+        maxCount: 1
+    },
+    {
+        name: "videoLocalPath",
+        maxCount: 1
+    }
+]), videoController.update_Video_Thumbnail)
+
 
 
 export default videoRouter
